4e069eb3b1abcf845dc8ff1e56d089bee43a5710
digivisor <digivisoragency@gmail.com>
2025-09-23 16:57:04 +0300

    vercel_config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true,
+    domains: ['images.pexels.com', 'www.w3schools.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https', 
+        hostname: 'www.w3schools.com',
+      }
+    ]
+  },
 };
 
 module.exports = nextConfig;
